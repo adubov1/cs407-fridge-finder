6c622856646e3a8bfc7692a27a3f9b1ab468e43e
Alex Dubov <dubov@wisc.edu>
2024-11-19 13:52:04 -0600

    add navigation between main, saved recipes, and camera

--- app/src/main/java/com/cs407/fridgefinder/SavedRecipesActivity.kt
+++ app/src/main/java/com/cs407/fridgefinder/SavedRecipesActivity.kt
@@ -2,35 +2,27 @@ package com.cs407.fridgefinder
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SavedRecipesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_saved_recipes)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        // Navigate to saved recipes when its button is clicked
-        val savedRecipesButton = findViewById<Button>(R.id.savedRecipesButton)
-        savedRecipesButton.setOnClickListener {
-            val intent = Intent(this, SavedRecipesActivity::class.java)
-            startActivity(intent)
-        }
-
-        // Navigate to scan camera activity when its button is clicked
-        val scanCameraButton = findViewById<ImageButton>(R.id.cameraButton)
-        scanCameraButton.setOnClickListener {
-            val intent = Intent(this, ScanCameraActivity::class.java)
+        // Navigate to main activity when back button is clicked
+        val backButton = findViewById<ImageButton>(R.id.backButton)
+        backButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
     }
