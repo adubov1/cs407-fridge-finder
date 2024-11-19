0199cfcf8c59e49805941ec5d56eeb3eaa7b602d
Alex Dubov <dubov@wisc.edu>
2024-11-19 11:38:17 -0600

    add build scripts and manifest

--- app/src/main/java/com/cs407/fridgefinder/RecipeListActivity.kt
+++ app/src/main/java/com/cs407/fridgefinder/RecipeListActivity.kt
@@ -1,4 +1,4 @@
-package com.cs407.fridge_finder
+package com.cs407.fridgefinder
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class RecipeListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_recipe_list)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
